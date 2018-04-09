98cbd55b191c36f9960a3f599890b60fa7b578db
Swingyboy <kedonosec@gmail.com>
2018-04-09 22:36:35 +0300

    Add population colors

--- App2/App2_src/App2.py
+++ App2/App2_src/App2.py
@@ -9,7 +9,8 @@ def set_color(elevation):
     else:
         return "red"
 
-data = pandas.read_csv("E:\\my_folder\\python\\App2\\App2_src\\Volcanoes_USA.txt")
+path = "E:\\my_folder\\python\\Python_repo\\App2\\App2_src\\"
+data = pandas.read_csv(path + "Volcanoes_USA.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
@@ -21,6 +22,11 @@ fg = folium.FeatureGroup(name = "My map")
 for lt, ln, el in zip(lat, lon, elev):
     fg.add_child(folium.CircleMarker(location = [lt, ln], radius = 6, color = "grey", fill_color = set_color(el),
                                      fill = True, fill_opacity = 0.7,  popup = str(el) + " m" ))
+
+fg.add_child(folium.GeoJson(data = (open(path + "world.json", 'r', encoding = 'utf-8-sig').read()),
+                            style_function = lambda x: {"fillColor":"green" if x["properties"]["POP2005"] < 10000000
+                                                        else "orange" if x["properties"]["POP2005"] < 60000000
+                                                        else "red"}))
 
 map.add_child(fg)
 
