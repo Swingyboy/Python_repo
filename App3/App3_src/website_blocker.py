adc0b361d45c1a53e8419be0918bf7d56f2f73e0
Swingyboy <kedonosec@gmail.com>
2018-06-03 11:13:29 +0300

    my test commit

--- App3/App3_src/website_blocker.py
+++ App3/App3_src/website_blocker.py
@@ -4,3 +4,8 @@ from datetime import datetime as dt
 host_path = "C:/Windows/System32/drivers/etc/"
 redirect = "127.0.0.1"
 website_list = ["www.facebook.com", "facebook.com"]
+
+while True:
+    if
+    print (1)
+    time.sleep(5)
