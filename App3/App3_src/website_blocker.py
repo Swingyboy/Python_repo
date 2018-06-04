955e12b9181b404477cbe24c12cee463adeb66f8
Swingyboy <kedonosec@gmail.com>
2018-06-04 23:58:22 +0300

    website check added.

--- App3/App3_src/website_blocker.py
+++ App3/App3_src/website_blocker.py
@@ -6,6 +6,16 @@ redirect = "127.0.0.1"
 website_list = ["www.facebook.com", "facebook.com"]
 
 while True:
-    if
-    print (1)
+    if dt(dt.now().year, dt.now().month, dt.now.day(), 8) < dt.now() < dt(
+            dt.now().year, dt.now().month, dt.now.day(), 16):
+        print("Working hours...")
+        with open(host_path, 'r+') as file:
+            content = file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(website)
+    else:
+        print("Fun hours...")
     time.sleep(5)
